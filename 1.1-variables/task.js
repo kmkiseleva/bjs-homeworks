5d7022ae4ef292b08e58789395ac1a0d0542ed34
Ksenia Kiseleva <ksushakiseleva@mail.ru>
2020-12-14 19:54:09 +0300

    first commit variables

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -6,22 +6,27 @@
  */
 
 function averageMark(a, g, p) {
-	
-    // реализуйте здесь задание №1
+	let algebra = a;
+	let geography = g;
+	let physics = p;
+	let count = 3; //число предметов
+   let average = (algebra + geography + physics) / count;
     
-	return // ваш ответ;
+	return average;
 }
 
-function sayHello(userName) {
-	
-    // реализуйте здесь задание №2
+function sayHello(userName) {	
+	let myName = userName;
+	let message = (`Привет, мир! Меня зовут ${userName}`)
     
-	return // ваш ответ;
+	return message;
 }
 
 function calculateFormula() {
+	let x = 2;
+	let y = 22;
+	let z = 0;
+	let result = x * y + 5 * z + x - 1;	
     
-	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+   return result;
 }
