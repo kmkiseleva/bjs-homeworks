7ebd7067e8277df5598c141212cad9c0aadceb01
Ksenia Kiseleva <ksushakiseleva@mail.ru>
2020-12-21 21:20:26 +0300

    second commit

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -4,16 +4,14 @@ function getResult(a, b, c) {
     let x = [];
     if (d < 0) {
         console.log('корней нет');
-        return x;
     } else if (d === 0) {
         console.log('один корень');
         x = [-b / 2 * a];
-        return x;
     } else if (d > 0) {
         console.log('два корня');
         x = [(-b + d ** (1 / 2)) / 2 * a, (-b - d ** (1 / 2)) / 2 * a];
-        return x;
     }
+    return x;
 }
 
 function getAverageMark(marks) {
@@ -22,46 +20,32 @@ function getAverageMark(marks) {
     let averageMark;
 
     if (amount === 0) {
-        console.log('количество оценок: ' + amount);
-        console.log('сумма оценок: ' + sum);
         return 0;
-    }
-    else if (amount > 5) {
-        console.log('количество оценок больше пяти');
+    } else if (amount > 5) {
+        console.log('количество введенных оценок больше пяти');
         marks = marks.slice(0, 5);
-        for (let i = 0; i < 5; i++) {
-            sum += marks[i];
-        }
-        averageMark = sum / 5;
-        console.log('количество оценок: ' + amount);
-        console.log('сумма первых пяти оценок: ' + sum);
-        console.log('средняя оценка: ' + averageMark);
-        return averageMark;
-    } else {
-        for (let i = 0; i < amount; i++) {
-            sum += marks[i];
-        }
-        averageMark = sum / amount;
-        console.log('количество оценок: ' + amount);
-        console.log('сумма оценок: ' + sum);
-        console.log('средняя оценка: ' + averageMark);
-        return averageMark;
+        amount = 5;
     }
+
+    for (let i = 0; i < amount; i++) {
+        sum += marks[i];
+    }
+
+    averageMark = sum / amount;
+    console.log('количество оценок: ' + amount);
+    console.log('сумма оценок: ' + sum);
+    console.log('средняя оценка: ' + averageMark);
+    return averageMark;
 }
 
 function askDrink(name, dateOfBirthday) {
-    let today = new Date();
-    let year = today.getFullYear();
-    let yearOfBirthday = dateOfBirthday.getFullYear();
-    let result = year - yearOfBirthday;
-    console.log(today);
-    console.log(year);
-    console.log(yearOfBirthday);
-    if (result > 18) {
-        alert('Не желаете ли олд-фэшн, ' + name + '?');
-        return result;
+    let result = new Date().getFullYear() - dateOfBirthday.getFullYear();
+
+    if (result >= 18) {
+        return ('Не желаете ли олд-фэшн, ' + name + '?');
+
     } else if (result < 18) {
-        alert('Сожалею, ' + name + ',но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!');
-        return result;
+        return ('Сожалею, ' + name + ',но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!');
     }
+    return result;
 }
