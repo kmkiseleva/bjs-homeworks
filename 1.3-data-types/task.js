562092fcce15cdb32bf7f4d53d440957682b2737
Ksenia Kiseleva <ksushakiseleva@mail.ru>
2020-12-22 22:00:59 +0300

    first commit wrappers-for-primitives

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,21 @@
+"use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let percentRate = percent / 100;
+    let credit = amount - contribution;
+    let months = (date.getFullYear() - new Date().getFullYear()) * 12 + (date.getMonth() - new Date().getMonth());
+    let monthPayment = credit * (percentRate / 12 + percentRate / (12 * (((1 + percentRate / 12) ** months) - 1)));
+    let totalAmount = months * monthPayment;
+    console.log(totalAmount.toFixed(2));
+    return totalAmount.toFixed(2);
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
+    if (name === '') {
+        console.log(`Привет, мир! Меня зовут Аноним.`);
+        return (`Привет, мир! Меня зовут Аноним.`);
+    } else {
+        console.log(`Привет, мир! Меня зовут ${name}.`);
+        return (`Привет, мир! Меня зовут ${name}.`);
+    }
     // return greeting;
 }
