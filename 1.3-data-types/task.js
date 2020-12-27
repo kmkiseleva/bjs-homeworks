ac49e9a6357e76ee8ba24b979b23ba8724990045
Ksenia Kiseleva <ksushakiseleva@mail.ru>
2020-12-27 23:09:25 +0300

    third

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,10 +1,27 @@
 "use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    const percentRate = percent / 100;
-    const credit = amount - contribution;
+    // проверка
+    if (isNaN(+percent)) {
+        return `Параметр <Процентная ставка> содержит неправильное значение <${percent}>`;
+    }
+    const parsedPercent = parseFloat(percent);
+    if (isNaN(+contribution)) {
+        return `Параметр <Сумма первоначального взноса> содержит неправильное значение <${contribution}>`;
+    }
+    const parsedContribution = parseFloat(contribution);
+
+    if (isNaN(+amount)) {
+        return `Параметр <Сумма кредита> содержит неправильное значение <${amount}>`;
+    }
+    const parsedAmount = parseFloat(amount);
+
+    // расчеты
+    const percentRate = parsedPercent / 100;
+    const credit = parsedAmount - parsedContribution;
     const months = (date.getFullYear() - new Date().getFullYear()) * 12 + (date.getMonth() - new Date().getMonth());
     const monthPayment = credit * (percentRate / 12 + percentRate / (12 * (((1 + percentRate / 12) ** months) - 1)));
     const totalAmount = months * monthPayment;
+
     console.log(totalAmount.toFixed(2));
     return totalAmount.toFixed(2);
 }
