f37394c5d562d503ed01fc8241f724ec1efd3290
Ksenia Kiseleva <ksushakiseleva@mail.ru>
2021-01-14 21:27:19 +0300

    first commit oop-es6

---
+++ 2.3-oop-es6/task.js
@@ -0,0 +1,156 @@
+"use strict";
+console.log("Задача 1");
+
+class PrintEditionItem {
+   constructor(name, releaseDate, pagesCount) {
+      this.name = name;
+      this.releaseDate = releaseDate;
+      this.pagesCount = pagesCount;
+      this.state = 100;
+      this.type = null;
+   }
+
+   fix() {
+      this.state *= 1.5;
+      return this.state;
+   }
+
+   set state(number) {
+      if (number < 0) {
+         this._state = 0;
+      } else if (number > 100) {
+         this._state = 100;
+      } else {
+         this._state = number;
+      }
+   }
+
+   get state() {
+      return this._state;
+      }
+}
+
+const sherlock = new PrintEditionItem("Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе", 2019, 1008);
+
+console.log(sherlock.releaseDate);
+console.log(sherlock.state);
+sherlock.fix();
+console.log(sherlock.state);
+
+class Magazine extends PrintEditionItem {
+   constructor(name, releaseDate, pagesCount) {
+      super(name, releaseDate, pagesCount)
+      this.type = "magazine";
+      }
+}
+
+class Book extends PrintEditionItem {
+   constructor(author, name, releaseDate, pagesCount) {
+      super(name, releaseDate, pagesCount)
+      this.author = author;
+      this.type = "book";
+      }
+}
+
+class NovelBook extends Book {
+   constructor(author, name, releaseDate, pagesCount) {
+      super(author, name, releaseDate, pagesCount)
+      this.type = "novel";
+   }
+}
+
+class FantasticBook extends Book {
+   constructor(author, name, releaseDate, pagesCount) {
+      super(author, name, releaseDate, pagesCount)
+      this.type = "fantastic";
+   }
+}
+
+class DetectiveBook extends Book {
+   constructor(author, name, releaseDate, pagesCount) {
+      super(author, name, releaseDate, pagesCount)
+      this.type = "detective";
+   }
+}
+
+const picknick = new FantasticBook("Аркадий и Борис Стругацкие", "Пикник на обочине", 1972, 168);
+
+console.log(picknick.author);
+picknick.state = 10;
+console.log(picknick.state);
+picknick.fix();
+console.log(picknick.state);
+
+console.log("\nЗадача 2");
+
+class Library {
+   constructor(name) {
+      this.name = name;
+      this.books = [];
+   }
+
+   addBook(book) {
+      if (book.state > 30) {
+         this.books.push(book);
+      }      
+   }
+
+   findBookBy(type, value) {
+      for (let i = 0; i < this.books.length; i++) {
+         if (this.books[i].type === value) {
+            return this.books[i];
+         } else {
+            return null;
+         }
+      }
+   }
+
+   findBookByName(bookName) {
+      if (this.books.includes(bookName)) {
+         let index = this.books.indexOf(bookName);
+         if (index > -1) {
+            this.books.splice(index, 1);
+            return this.books[index];
+         }         
+      } else {
+         return null;
+      }
+   }
+}
+
+const library = new Library("Библиотека имени Ленина");
+
+library.addBook(new DetectiveBook("Артур Конан Дойл", "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе", 2019, 1008));
+library.addBook(new FantasticBook("Аркадий и Борис Стругацкие", "Пикник на обочине", 1972, 168));
+library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
+library.addBook(new Magazine("Мурзилка", 1924, 60));
+
+console.log(library);
+console.log("Количество книг до выдачи: " + library.books.length);
+
+console.log("\nЗадача 3");
+
+class StudentLog {
+   constructor(name) {
+      this.name = name;
+   }
+
+   getName() {
+      return this.name;
+   }
+
+   addGrade(grade, subject) {
+
+   }
+
+   getAverageBySubject(subject) {
+
+   }
+
+   getTotalAverage() {
+
+   }
+}
+
+const log = new StudentLog('Олег Никифоров');
+console.log(log.getName());
